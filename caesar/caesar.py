16ceaba637fdd4071a9bf96276aa9d80041876c8
Nicolas Agustin Torres <nicolastrres@gmail.com>
2015-05-11 10:14:19 -0300

    Refactoring

--- caesar/caesar.py
+++ caesar/caesar.py
@@ -4,10 +4,14 @@ class Caesar():
 
     @classmethod
     def encrypt(cls, message, key):
-        encripted_message = ""
+        encrypted_message = ""
         for letter in message:
-            number_letter = ord(letter) + key
-            number_after_last_letter = number_letter - 122
-            encripted_letter = number_letter if number_after_last_letter <= 0 else 96 + number_after_last_letter
-            encripted_message += chr(encripted_letter)
-        return encripted_message
+            encrypted_message += cls.encrypt_letter(letter, key)
+        return encrypted_message
+
+    @classmethod
+    def encrypt_letter(self, letter, key):
+        number_letter = ord(letter) + key
+        number_after_last_letter = number_letter - 122
+        encrypted_letter = number_letter if number_after_last_letter <= 0 else 96 + number_after_last_letter
+        return chr(encrypted_letter)
