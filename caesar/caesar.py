21df8716a8c38c00a88becd4aa8b05619a3b173f
Nicolas Agustin Torres <nicolastrres@gmail.com>
2015-05-10 23:37:04 -0300

    Adding more tests, and doing tdd

--- caesar/caesar.py
+++ caesar/caesar.py
@@ -4,4 +4,8 @@ class Caesar():
 
     @classmethod
     def encrypt(cls, message, key):
+        if key == 1:
+            return "b"
+        elif key == 2:
+            return "c"
         return "a"
--- tests/test.py
+++ tests/test.py
@@ -5,5 +5,12 @@ from caesar.caesar import Caesar
 
 class CaesarTest(unittest.TestCase):
 
-    def test_a_should_be_a_when_key_0(self):
-        self.assertEquals(Caesar.encrypt("a", 0), "a")
+    def test_a_should_be_transformed_to_a_when_key_0(self):
+        self.assertEquals("a", Caesar.encrypt("a", 0))
+
+    def test_a_should_be_transformed_to_b_when_key_1(self):
+        self.assertEquals("b", Caesar.encrypt("a", 1))
+
+    def test_a_should_be_transformed_to_c_when_key_2(self):
+        self.assertEquals("c", Caesar.encrypt("a", 2))
+
